d20f463b7f278d75bd6b913cc0c95255d3b741bd
StephenCole19 <stephencole139@gmail.com>
2019-11-05 11:15:55 -0400

    Made some progress

--- Python/A4/read_csv.py
+++ Python/A4/read_csv.py
@@ -1,21 +1,53 @@
 import csv
 
-with open("2015-100.csv") as file:
-    arr = []
-    reader = csv.reader(file, delimiter=",")
-    count = 0
+def read_csv(csvF):
+    with open(csvF) as file:
+        arr = []
+        reader = csv.reader(file, delimiter=",")
 
-    for row in reader:
-
-        r = []
-        i = 0
-        while(i < len(row)):
-            r.append(row[count])
-            i += 1
+        for row in reader:
+            arr.append(row)
         
-        arr.append(r)
-        r = []
-        i = 0
-            
+        return arr
+
+
+table = read_csv("test.csv")
+
+def header_map():
+    hmap = {}
+    i = len(table) - 1
+
+    for x in table[0]:
+        hmap[i] = x
+        i -= 1
+
+    return hmap
+
+
+def select(table, cols):
+    arr = []
+    r = []
+
+    count = 0
+    hm = header_map()
+
+    for x in cols:
+        count = 0
+        while(count < len(table)-1):
+            if(x == table[0][count]):
+                r.append(count)
+            count += 1
+
+    for row in table:
+        cat = []
+        for x in r:
+            cat.append(row[x])
+
+        arr.append(cat)
+
     print(arr)
 
+
+
+
+select(table, {'name', "eye_colour"})
